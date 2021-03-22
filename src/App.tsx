cb1bebb179bca1f131e6414596c185c6922ff9f6
Ean <eanesparrago@gmail.com>
2021-03-22 15:41:28 +0800

    setup styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import styled, { ThemeProvider } from "styled-components";
+import "destyle.css";
+
+import theme from "styles/theme";
+
 function App() {
-  return <div>test</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <S.App>test 2</S.App>
+    </ThemeProvider>
+  );
 }
+
+const S = {} as any;
+
+S.App = styled.div`
+  background-color: red;
+  color: ${(p) => p.theme.color.white};
+`;
 
 export default App;
